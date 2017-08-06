277fc9858f098ceff60d9fa56c89ca5817921273
Devashish2910 <dgpatel2910@gmail.com>
2017-08-06 00:41:17 -0500

    Update: Style in Model Popup

--- src/components/common/Confirm.js
+++ src/components/common/Confirm.js
@@ -5,6 +5,7 @@ import {Card} from './Card';
 
 class Confirm extends Component {
   render() {
+    const {cardSection, textStyle, containerStyle} = styles;
     return (
       <Model
         visible={this.props.visible}
@@ -13,10 +14,10 @@ class Confirm extends Component {
         transperent
         style={{...this.props.style}}
         >
-        <View>
-          <CardSection>
-            <Text>
-              {this.props.notice}
+        <View style={containerStyle}>
+          <CardSection style={cardSectionStyle}>
+            <Text style={textStyle}>
+              {this.props.warning}
             </Text>
           </CardSection>
           <CardSection>
@@ -30,6 +31,23 @@ class Confirm extends Component {
 }
 
 const styles = {
+  cardSectionStyle = {
+    justifyContent: 'center'
+
+  },
+  textStyle= {
+    flex: 1,
+    fontSize: 18,
+    textAlign: 'center',
+    lineHeight: 40
+  },
+  containerStyle = {
+    backgroundColor: 'rgba(0, 0, 0, 0.75)',
+    position: 'relative',
+    flex: 1,
+    justifyContent: 'center'
+
+  }
 
 }
 
